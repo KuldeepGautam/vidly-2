35f7f8b6d86b9568c88d3c0f4f51f2f02d5d89de
Kuldeep Gautam <kuldeep1071996@gmail.com>
2023-02-15 23:51:13 +0530

    bind method

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,27 +1,50 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-    state = {
-        count: 1
-    };
+  state = {
+    count: 1,
+  };
 
-    render() {
-        let classes = 'badge m-1 bg-';
-        classes += this.state.count === 0 ? 'warning' : 'primary';
+  //   renderTags() {
+  //     if (this.state.tags.length === 0) return "<p>There are no data</p>";
+  //     return (
+  //       <ul>
+  //         {this.state.tags.map((tag) => (
+  //           <li key={tag}>{tag}</li>
+  //         ))}
+  //       </ul>
+  //     );
+  //   }
 
-        return (
-            <>
-                <div class="container">
-                    <span className={classes}>{this.formateCount()}</span>
-                    <button className="btn btn-primary">Increment</button>
-                </div>
-            </>
-        );
-    }
-    formateCount() {
-        const { count } = this.state;
-        return count === 0 ? 'Zero' : count;
-    }
+  handleIncrement = () => {
+    console.log("Handle Increment", this);
+  };
+
+  render() {
+    return (
+      <>
+        <div className="container">
+          {/* {this.state.tags.length === 0 && "Please create a new tags"}
+          <div>{this.renderTags()}</div> */}
+          <span className={this.getBadgeClasses()}>{this.formateCount()}</span>
+          <button onClick={this.handleIncrement} className="btn btn-primary">
+            Increment
+          </button>
+        </div>
+      </>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-1 bg-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formateCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
 }
 
 export default Counter;
